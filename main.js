363327d0f1f06dfd491330b2c3cb494c29a7bd0e
jumi sanchez <jumary.sanchez@codeimmersives.com>
2020-04-08 10:18:20 -0400

    method

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const newPerson = function(firstName = 'Anonymous', lastName = 'Person', age, married = false){
+  
+  const newPerson = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+    married: married,
+
+    goingOn: function(){
+      newAge = this.age
+    return newAge + 1
+    },
+
+    ageUp: function(){
+    this.age = this.age + 1
+    },
+    getFullName: function(){
+      return this.firstName + this.lastName
+    },
+    marry: function(person2){
+      this.married = true
+      person2.married = true
+      
+    },
+
+    divorce: function(person2){
+      this.married = false
+      delete this.spouseName
+      person2.married = false
+      delete person2.spouseName
+    }
+  };
+return newPerson;
+};
+
+
+
 
 
 
